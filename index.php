fa6db37e3c4cc9ebcabd83a610ca6809750be87c
mrpipro <83968916+mrpipro@users.noreply.github.com>
2021-05-12 00:35:03 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- wrapper -->
+<div class="wrapper">
+     <h1>Bike Stock</h1>
+ <span><i class="shopping-cart"></i></span>
+
+    <div class="clear"></div>
+    <!-- items -->
+    <div class="items">
+        <!-- single item -->
+        <div class="item">
+            <img src="https://picsum.photos/200" alt="item" />
+             <h2>Item 1</h2>
+
+            <p>Price: <em>$449</em>
+            </p>
+            <button class="add-to-cart" type="button">Add to cart</button>
+        </div>
+        <!--/ single item -->
+    </div>
+    <!--/ items -->
+</div>
+<!--/ wrapper -->
